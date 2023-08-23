7ec54f49ca02ab54f53490e45360436c19fe2f07
Bogdan Covrig <bogcov@gmail.com>
2023-08-23 16:43:17 +0200

    feat: Add product grid layout

--- admin/src/components/ProductGrid/index.js
+++ admin/src/components/ProductGrid/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import {
   Field,
@@ -6,7 +6,20 @@ import {
   FieldHint,
   FieldError,
   Flex,
+  Box,
+  GridLayout,
+  Card,
+  CardHeader,
+  CardCheckbox,
+  CardAsset,
+  CardBody,
+  CardContent,
+  CardTitle,
+  CardSubtitle,
+  Loader,
 } from "@strapi/design-system";
+
+import { request } from "@strapi/helper-plugin";
 
 import { useIntl } from "react-intl";
 
@@ -22,6 +35,22 @@ export default function ProductGrid({
 }) {
   const { formatMessage } = useIntl();
 
+  const [productData, setProductData] = useState([]);
+
+  const fetchData = async () => {
+    const data = await request("/commercetools/getAllProducts", {
+      method: "GET",
+    });
+
+    console.log("data", data);
+
+    setProductData(data);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <Field name={name} id={name} error={error} hint={description}>
       <Flex justifyContent="space-between" paddingBottom={2}>
@@ -29,7 +58,31 @@ export default function ProductGrid({
           {formatMessage(intlLabel)}
         </FieldLabel>
       </Flex>
-      <h1>hey!!!!</h1>
+      <Box padding={8} background="neutral100">
+        {productData.length > 0 ? (
+          <GridLayout>
+            {productData.map((product) => (
+              <Card id={`product-${product.id}`}>
+                <CardHeader>
+                  <CardCheckbox value />
+                  <CardAsset src={product.image} />
+                </CardHeader>
+                <CardBody>
+                  <CardContent>
+                    <CardTitle>{product.title}</CardTitle>
+                    <CardSubtitle>R{product.price}</CardSubtitle>
+                  </CardContent>
+                  {/* <CardBadge>Doc</CardBadge> */}
+                </CardBody>
+              </Card>
+            ))}
+          </GridLayout>
+        ) : (
+          <Flex alignItems="center" justifyContent="center">
+            <Loader>Loading content...</Loader>
+          </Flex>
+        )}
+      </Box>
       <FieldHint />
       <FieldError />
     </Field>
