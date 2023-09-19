2b404f439f3a9d8809dfeeb462223f74d9372867
Bogdan Covrig <bogcov@gmail.com>
2023-09-19 14:49:46 +0200

    fix: Update plugin for CustomFieldServerOptions

--- dist/server/register.js
+++ dist/server/register.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = ({ strapi }) => {
     strapi.customFields.register({
         name: 'ProductGrid',
-        pluginId: 'commercetools',
+        plugin: 'commercetools',
         type: 'text',
         inputSize: {
             default: 12,
