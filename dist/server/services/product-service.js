d42e122fe3965d1820b5ebe26fcbd6385b6b705b
Bogdan Covrig <bogcov@gmail.com>
2023-09-21 16:41:22 +0200

    feat: Add commercetoools projections

--- dist/server/services/product-service.js
+++ dist/server/services/product-service.js
@@ -1,21 +1,22 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const data_1 = require("./data");
 const client_1 = require("./commercetools/client");
+const { CT_DEFAULT_LOCALE = 'en-ZA' } = process.env;
 exports.default = ({ strapi }) => ({
     async getAllProducts() {
-        const products = await client_1.client.products().get().execute();
+        const products = await client_1.client.productProjections().get().execute();
         const productsData = products.body.results.map((product) => {
-            var _a, _b, _c;
-            const name = product.masterData.current.name['en-ZA'];
-            const description = (_a = product.masterData.current.description) === null || _a === void 0 ? void 0 : _a['en-ZA'];
-            const sku = product.key;
-            const image = (_b = product.masterData.current.masterVariant.images) === null || _b === void 0 ? void 0 : _b[0].url;
-            const price = (_c = product.masterData.current.masterVariant.prices) === null || _c === void 0 ? void 0 : _c[0].value.centAmount;
+            var _a, _b, _c, _d;
+            // TODO: WIP validation
+            const name = product.name[CT_DEFAULT_LOCALE];
+            const description = (_a = product.description) === null || _a === void 0 ? void 0 : _a[CT_DEFAULT_LOCALE];
+            const slug = (_b = product.slug) === null || _b === void 0 ? void 0 : _b[CT_DEFAULT_LOCALE];
+            const image = (_c = product.masterVariant.images) === null || _c === void 0 ? void 0 : _c[0].url;
+            const price = (_d = product.masterVariant.prices) === null || _d === void 0 ? void 0 : _d[0].value.centAmount;
             return {
                 title: name,
                 description,
-                id: sku,
+                id: slug,
                 image,
                 price,
             };
@@ -23,21 +24,29 @@ exports.default = ({ strapi }) => ({
         return productsData;
     },
     async getProductById(id) {
-        var _a, _b, _c;
-        const product = await client_1.client.products().withKey({ key: id }).get().execute();
-        console.log(product);
-        const name = product.body.masterData.current.name['en-ZA'];
-        const description = (_a = product.body.masterData.current.description) === null || _a === void 0 ? void 0 : _a['en-ZA'];
-        const sku = product.body.key;
-        const image = (_b = product.body.masterData.current.masterVariant.images) === null || _b === void 0 ? void 0 : _b[0].url;
-        const price = (_c = product.body.masterData.current.masterVariant.prices) === null || _c === void 0 ? void 0 : _c[0].value.centAmount;
+        var _a, _b, _c, _d;
+        const product = await client_1.client
+            .productProjections()
+            .get({
+            queryArgs: {
+                where: `slug(${CT_DEFAULT_LOCALE}="${id}")`,
+            },
+        })
+            .execute();
+        // TODO: WIP validation
+        const productData = product.body.results[0];
+        // TODO: WIP validation
+        const name = productData.name[CT_DEFAULT_LOCALE];
+        const description = (_a = productData.description) === null || _a === void 0 ? void 0 : _a[CT_DEFAULT_LOCALE];
+        const slug = (_b = productData.slug) === null || _b === void 0 ? void 0 : _b[CT_DEFAULT_LOCALE];
+        const image = (_c = productData.masterVariant.images) === null || _c === void 0 ? void 0 : _c[0].url;
+        const price = (_d = productData.masterVariant.prices) === null || _d === void 0 ? void 0 : _d[0].value.centAmount;
         return {
             title: name,
             description,
-            id: sku,
+            id: slug,
             image,
             price,
         };
-        return data_1.DUMMY_DATA.find((product) => product.id === id);
     },
 });
--- server/services/product-service.ts
+++ server/services/product-service.ts
@@ -1,24 +1,25 @@
 import { Strapi } from '@strapi/strapi';
 
-import { DUMMY_DATA as data } from './data';
-
 import { client } from './commercetools/client';
+
+const { CT_DEFAULT_LOCALE = 'en-ZA' } = process.env;
 
 export default ({ strapi }: { strapi: Strapi }) => ({
   async getAllProducts() {
-    const products = await client.products().get().execute();
+    const products = await client.productProjections().get().execute();
 
     const productsData = products.body.results.map((product) => {
-      const name = product.masterData.current.name['en-ZA'];
-      const description = product.masterData.current.description?.['en-ZA'];
-      const sku = product.key;
-      const image = product.masterData.current.masterVariant.images?.[0].url;
-      const price = product.masterData.current.masterVariant.prices?.[0].value.centAmount;
+      // TODO: WIP validation
+      const name = product.name[CT_DEFAULT_LOCALE];
+      const description = product.description?.[CT_DEFAULT_LOCALE];
+      const slug = product.slug?.[CT_DEFAULT_LOCALE];
+      const image = product.masterVariant.images?.[0].url;
+      const price = product.masterVariant.prices?.[0].value.centAmount;
 
       return {
         title: name,
         description,
-        id: sku,
+        id: slug,
         image,
         price,
       };
@@ -28,23 +29,31 @@ export default ({ strapi }: { strapi: Strapi }) => ({
   },
 
   async getProductById(id: string) {
-    const product = await client.products().withKey({ key: id }).get().execute();
-    console.log(product);
+    const product = await client
+      .productProjections()
+      .get({
+        queryArgs: {
+          where: `slug(${CT_DEFAULT_LOCALE}="${id}")`,
+        },
+      })
+      .execute();
 
-    const name = product.body.masterData.current.name['en-ZA'];
-    const description = product.body.masterData.current.description?.['en-ZA'];
-    const sku = product.body.key;
-    const image = product.body.masterData.current.masterVariant.images?.[0].url;
-    const price = product.body.masterData.current.masterVariant.prices?.[0].value.centAmount;
+    // TODO: WIP validation
+    const productData = product.body.results[0];
+
+    // TODO: WIP validation
+    const name = productData.name[CT_DEFAULT_LOCALE];
+    const description = productData.description?.[CT_DEFAULT_LOCALE];
+    const slug = productData.slug?.[CT_DEFAULT_LOCALE];
+    const image = productData.masterVariant.images?.[0].url;
+    const price = productData.masterVariant.prices?.[0].value.centAmount;
 
     return {
       title: name,
       description,
-      id: sku,
+      id: slug,
       image,
       price,
     };
-
-    return data.find((product) => product.id === id);
   },
 });
