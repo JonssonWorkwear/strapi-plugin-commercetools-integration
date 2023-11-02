40965c38fd1e20a291dab29c85d41ad31a35c11f
Bogdan Covrig <bogcov@gmail.com>
2023-11-02 12:46:18 +0100

    fix: Remove dist from code

--- dist/server/services/category-service.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const client_1 = require("./commercetools/client");
-exports.default = () => ({
-    async getAllCategories() {
-        return client_1.client.categories().get().execute();
-    },
-});
